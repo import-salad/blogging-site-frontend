03ff42d73d7f6a0cf7195464d8336ce378428966
import-salad <shivendushishir@gmail.com>
2020-12-04 12:28:56 +0530

    updated Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,9 @@
 import React, {Component} from "react";
 import {Container, Row, Col} from "reactstrap";
 
+import NavBar from "./navigation/NavBar.js";
+import SideBar from "./navigation/SideBar.js";
+
 class Dashboard extends Component
 {
     render()
@@ -8,9 +11,23 @@ class Dashboard extends Component
         return(
 
             <div className="dashboard">
-                <Container>
-                    <Row className="label">
-                        <Col className="label-content">Welcome, {this.props.match.params.email}!!!</Col>
+                <NavBar isUserLogged={false} />
+                <Container
+                    style = 
+                    {
+                        {
+                            maxWidth: "90%",
+                            marginLeft: "0px",
+                            marginRight: "0px",
+                            marginTop: "10px"
+                        }
+                    }
+                >
+                    <Row>
+                        <SideBar />
+                        <Col>
+                            {"USER-ID: " + this.props.match.params.userID}
+                        </Col>
                     </Row>
                 </Container>
             </div>
